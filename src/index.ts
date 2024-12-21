af1bc0451b517a325420aacdfd554ef3c9db7121
liuwei <wei2460@qq.com>
2024-12-21 15:25:20 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,27 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import YAML from 'yaml'
+
+const BITNAMI_REPOSITORY_URL = 'https://charts.bitnami.com';
+const DOCKER_REGISTRY = 'registry-1.docker.io';
+const MIRRIR_REGISTRY = 'docker.eastcoal.tech';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const { pathname, search, hash, username, password } = URL.parse(request.url) as URL
+		const targetUrl = new URL(BITNAMI_REPOSITORY_URL);
+		targetUrl.username = username;
+		targetUrl.password = password;
+		targetUrl.pathname = pathname;
+		targetUrl.search = search;
+		targetUrl.hash = hash;
+		const headers = new Headers(request.headers);
+		const response = await fetch(targetUrl, {
+			headers: headers,
+		})
+		const text = await response.text()
+		const result = text.replaceAll(DOCKER_REGISTRY, MIRRIR_REGISTRY)
+		return new Response(result);
 	},
 } satisfies ExportedHandler<Env>;
